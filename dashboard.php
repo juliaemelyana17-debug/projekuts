639c4a0dd28ec1708cb6527daf6ed50c6ec145c9
julia17 <juliaemelyana17@gmail.com>
2025-11-20 23:02:26 +0700

    Dashboard Penjualan (Diskon Belanja)

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,17 @@
 <?php
-// Commit 8
-echo "<h3>Total Belanja: Rp $grandtotal</h3>";
+// Commit 9: Diskon Belanja
+$diskon = 0;
+
+if ($grandtotal < 50000) {
+    $diskon = 0.05 * $grandtotal;
+} elseif ($grandtotal <= 100000) {
+    $diskon = 0.10 * $grandtotal;
+} else {
+    $diskon = 0.15 * $grandtotal;
+}
+
+$total_bayar = $grandtotal - $diskon;
+
+echo "<h3>Diskon: Rp $diskon</h3>";
+echo "<h2>Total Bayar: Rp $total_bayar</h2>";
 ?>
