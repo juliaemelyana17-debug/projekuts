420d1beda6651d76a4f9efbfc3a1bd0b98846259
julia17 <juliaemelyana17@gmail.com>
2025-11-20 23:00:45 +0700

    Dashboard Penjualan (Total Belanja)

--- dashboard.php
+++ dashboard.php
@@ -1,26 +1,4 @@
 <?php
-// Commit 7
-echo "<table border='1' cellpadding='8'>";
-echo "<tr>
-        <th>Kode</th>
-        <th>Nama</th>
-        <th>Harga</th>
-        <th>Jumlah</th>
-        <th>Total</th>
-      </tr>";
-
-foreach ($beli as $i => $item) {
-    $total[$i] = $harga_barang[$item] * $jumlah[$i];
-    $grandtotal += $total[$i];
-
-    echo "<tr>
-            <td>" . $kode_barang[$item] . "</td>
-            <td>" . $nama_barang[$item] . "</td>
-            <td>Rp " . $harga_barang[$item] . "</td>
-            <td>" . $jumlah[$i] . "</td>
-            <td>Rp " . $total[$i] . "</td>
-          </tr>";
-}
-
-echo "</table>";
+// Commit 8
+echo "<h3>Total Belanja: Rp $grandtotal</h3>";
 ?>
